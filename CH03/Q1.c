cc736a9fe989c2d3157c90f8350af7736132c54d
Tanshen <Onemoment1234567@outlook.com>
2025-04-27 10:44:58 +0530

    Update Q1.c

--- CH03/Q1.c
+++ CH03/Q1.c
@@ -4,13 +4,14 @@ Q. Write a C program to find the sum and average of given three numbers .
 
 */
 
-#include <conio.h> // for turboc++ compilers , remove if you are not using
+#include <conio.h> // for turboc++ compilers , remove if you are not using turboc++
 #include <stdio.h>
-// turboc++
+
 
 int main() {
 
-  int num1, num2, num3, sum = 0, average = 0;
+  int num1, num2, num3, sum = 0;
+  float average = 0;
 
   clrscr(); // remove if you are not using turboc++
 
@@ -18,9 +19,9 @@ int main() {
   scanf("%d %d %d", &num1, &num2, &num3);
 
   sum = num1 + num2 + num3;
-  average = (num1 + num2 + num3) / 3;
+  average = (num1 + num2 + num3) / 3.0;
 
-  printf("Sum : %d \nAverage : %d\n", sum, average);
+  printf("Sum : %d \nAverage : %6.2f\n", sum, average);
   getch() // remove if you are not using turboc++
 
       return 0;
