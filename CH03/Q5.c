3cd1d82fb98c972e43897ac40c0a4e8a65276458
tanxzen <Onemoment1234567@outlook.com>
2025-04-29 21:04:23 +0530

    Added Q5.c

---
+++ CH03/Q5.c
@@ -0,0 +1,24 @@
+/*
+
+Q5. Write a C program to find the ASCII character of a given integer.
+
+*/
+
+#include <conio.h> //remove if not using turboc++
+#include <stdio.h>
+
+int main() {
+
+  int n;
+
+  clrscr(); // remove if not using turboc++;
+
+  printf("Enter number : ");
+  scanf("%d", &n);
+
+  printf("Character : %c\n",
+         n); // we are converting the interger to charcter with the format
+             // specifier %c which represent charcter ;
+  getch();   // remove if not using turboc++;
+  return 0;
+}
