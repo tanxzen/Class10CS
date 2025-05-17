569c8fce87118e15e0ad287897153ec2773d56e2
tanxzen <Onemoment1234567@outlook.com>
2025-05-17 11:40:30 +0530

    Added website materials

---
+++ docs/script.js
@@ -0,0 +1,32 @@
+const text = "Class 10 CS – Study Guide";
+const target = document.getElementById("typewriter");
+
+let index = 0;
+let isDeleting = false;
+
+function typeWriterLoop() {
+  if (!isDeleting) {
+    target.textContent += text.charAt(index);
+    index++;
+    if (index < text.length) {
+      setTimeout(typeWriterLoop, 100); // typing speed
+    } else {
+      setTimeout(() => {
+        isDeleting = true;
+        typeWriterLoop();
+      }, 2000); // pause before deleting
+    }
+  } else {
+    target.textContent = text.substring(0, index - 1);
+    index--;
+    if (index > 0) {
+      setTimeout(typeWriterLoop, 50); // deleting speed
+    } else {
+      isDeleting = false;
+      setTimeout(typeWriterLoop, 500); // pause before typing again
+    }
+  }
+}
+
+window.onload = typeWriterLoop;
+
